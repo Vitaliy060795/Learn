6aea15676e39810ab7eb867898e8981af64d4e92
Vitaliy <vitaliy060795@gmail.com>
2019-08-08 00:34:09 +0300

    AddMethodWithMemoization

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,10 +24,11 @@ namespace ConsoleApp1
             Console.WriteLine("Select a function from the list and enter the corresponding number:");
             Console.WriteLine("1 - Find the Fibonacci number with recursion");
             Console.WriteLine("2 - Find the Fibonacci number without recursion");
-            Console.WriteLine("3 - Creating a graph using recursion");
-            if (int.TryParse(Console.ReadLine(), out int num) && num > 0 && num < 4)
+            Console.WriteLine("3 - Find the Fibonacci number with recursion (memoization)");
+            Console.WriteLine("4 - Creating a graph using recursion");
+            if (int.TryParse(Console.ReadLine(), out int num) && num > 0 && num < 5)
             {
-                if (num == 1 || num == 2)
+                if (num == 1 || num == 2  || num == 3)
                 {
                     int a = 0;
                     Console.WriteLine("Enter the number n");
@@ -46,10 +47,19 @@ namespace ConsoleApp1
                             a = fib_n(num2);
                             stopWatch.Stop();
                         }
+                        else if (num == 3)
+                        {
+                            stopWatch.Start();
+                            int[] results = new int[num2 + 1];
+                            results[0] = 0;
+                            results[1] = 1;
+                            a = fibonacchiWithMemoization(results, num2);
+                            stopWatch.Stop();
+                        }
                         Console.WriteLine(string.Format("Fibonacci number of order {0} = {1}" + '\n' + "Time taken: {2} ms", num2, a, stopWatch.Elapsed.TotalMilliseconds));
                     }
                 }
-                else if (num == 3)
+                else if (num == 4)
                 {
                     Console.WriteLine("Enter chart depth");
                     int.TryParse(Console.ReadLine(), out int deep);
@@ -69,14 +79,28 @@ namespace ConsoleApp1
                 Console.WriteLine("You entered an invalid value, try again");
             }
     }
-       
+
         static int fib(int n)
         {
             if (n < 2 ) return n;
             return fib(n - 1) + fib(n - 2);
         }
-
-       static int fib_n(int n)
+        
+        static int fibonacchiWithMemoization(int[] results, int n)
+        {
+            if (n == 0)
+                return results[0];
+            if(n == 1)
+                return results[1];
+            
+            if (results[n] == 0)
+            {
+                results[n] = fibonacchiWithMemoization(results,n - 2) + fibonacchiWithMemoization(results,n - 1);
+            }
+            return results[n];
+        }
+        
+        static int fib_n(int n)
         {
             int x = 1;
             int y = 0;
