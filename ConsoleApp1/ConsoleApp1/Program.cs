b8e885d0b7dbfc2bc9ffc6aa585ddae683ec5df6
Vitaliy <vitaliy060795@gmail.com>
2019-11-10 17:34:40 +0200

    nnn

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,74 +12,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            FirstNonConsecutive(new int[] { 1, 2, 3, 4, 7, 5, 6 });
             Console.Read();
-            while (true)
-            {
-                MainMethod();
-            }
+           
         }
 
-        static void MainMethod()
+
+        public static object FirstNonConsecutive(int[] arr)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            Console.WriteLine("Select a function from the list and enter the corresponding number:");
-            Console.WriteLine("1 - Find the Fibonacci number with recursion");
-            Console.WriteLine("2 - Find the Fibonacci number without recursion");
-            Console.WriteLine("3 - Find the Fibonacci number with recursion (memoization)");
-            Console.WriteLine("4 - Creating a graph using recursion");
-            if (int.TryParse(Console.ReadLine(), out int num) && num > 0 && num < 5)
+            int d = arr[0];
+            for (int i = 0; i < arr.Length; i++)
             {
-                if (num == 1 || num == 2  || num == 3)
-                {
-                    int a = 0;
-                    Console.WriteLine("Enter the number n");
-                    if (int.TryParse(Console.ReadLine(), out int num2))
-                    {
-                        if (num == 1)
-                        {
-                            stopWatch.Start();
-                            a = fib(num2);
-                            stopWatch.Stop();
-
-                        }
-                        else if (num == 2)
-                        {
-                            stopWatch.Start();
-                            a = fib_n(num2);
-                            stopWatch.Stop();
-                        }
-                        else if (num == 3)
-                        {
-                            stopWatch.Start();
-                            int[] results = new int[num2 + 1];
-                            results[0] = 0;
-                            results[1] = 1;
-                            a = fibonacchiWithMemoization(results, num2);
-                            stopWatch.Stop();
-                        }
-                        Console.WriteLine(string.Format("Fibonacci number of order {0} = {1}" + '\n' + "Time taken: {2} ms", num2, a, stopWatch.Elapsed.TotalMilliseconds));
-                    }
+                if (d != arr[i])
+               {
+                    Console.WriteLine(arr[i]);
+                    return arr[i];
                 }
-                else if (num == 4)
-                {
-                    Console.WriteLine("Enter chart depth");
-                    int.TryParse(Console.ReadLine(), out int deep);
-                    Console.WriteLine("Enter the number of children");
-                    int.TryParse(Console.ReadLine(), out int children);
-
-                    Class1 temp = new Class1();
-                    temp.CreateGraph(deep, children);
-                }
-                else
-                {
-                    Console.WriteLine("You entered an invalid value, try again");
-                }
+                d++;
             }
-            else
-            {
-                Console.WriteLine("You entered an invalid value, try again");
-            }
-    }
+            return null;
+        }
 
         static int fib(int n)
         {
